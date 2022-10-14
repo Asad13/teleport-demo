ddc69ee19271dffdb20739dad1edf359658a60f4
Asad Mujumder <asaduzzaman.anik.me@gmail.com>
2022-10-14 19:25:56 +0600

    Initial Structure

---
+++ src/routes/home.js
@@ -0,0 +1,355 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './home.css'
+
+const Home = (props) => {
+  return (
+    <div className="home-container">
+      <Helmet>
+        <title>Teleport Demo</title>
+      </Helmet>
+      <div className="home-sample-wireframe">
+        <div className="home-navigation-c">
+          <div className="home-sample-logo">
+            <span className="home-text">
+              <span>photo</span>
+            </span>
+            <img
+              alt="UnionI658"
+              src="/playground_assets/unioni658-wwj.svg"
+              className="home-union"
+            />
+            <img
+              alt="UnionI658"
+              src="/playground_assets/unioni658-u6t.svg"
+              className="home-union1"
+            />
+          </div>
+          <span className="home-text02">
+            <span>Features</span>
+          </span>
+          <span className="home-text04">
+            <span>Pricing</span>
+          </span>
+          <span className="home-text06">
+            <span>Community</span>
+          </span>
+          <span className="home-text08">
+            <span>Support</span>
+          </span>
+          <button className="home-button1">
+            <img
+              alt="Rectangle2I664"
+              src="/playground_assets/rectangle2i664-lh28f-200h.png"
+              className="home-rectangle21"
+            />
+            <span className="home-text12">
+              <span>Log in</span>
+            </span>
+          </button>
+        </div>
+        <div className="home-hero-e">
+          <img
+            alt="Rectangle666"
+            src="/playground_assets/rectangle666-lq3u.svg"
+            className="home-rectangle"
+          />
+          <div className="home-thelargestcommunityofphotoenthusiasts">
+            <span className="home-text14 heroH1">
+              <span>The largest community of photo enthusiasts</span>
+            </span>
+          </div>
+          <button className="home-button2">
+            <img
+              alt="Rectangle2I668"
+              src="/playground_assets/rectangle2i668-uk8-200h.png"
+              className="home-rectangle22"
+            />
+            <span className="home-text16">
+              <span>Join Today</span>
+            </span>
+          </button>
+          <div className="home-group">
+            <img
+              alt="EllipseI669"
+              src="/playground_assets/ellipsei669-twq-200h.png"
+              className="home-ellipse"
+            />
+            <img
+              alt="PolygonI669"
+              src="/playground_assets/polygoni669-n1sl.svg"
+              className="home-polygon"
+            />
+          </div>
+        </div>
+        <div className="home-body-b">
+          <span className="home-text18 sub-PageH1">
+            <span>Snap photos and share like never before</span>
+          </span>
+          <span className="home-text20 bodyH4">
+            <span>Sed ut perspiciatis</span>
+          </span>
+          <span className="home-text22 bodyP">
+            <span>
+              Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+              aut fugit, sed quia consequuntur magni dolores eos qui ratione
+              voluptatem sequi nesciunt. Neque porro quisquam est.
+            </span>
+          </span>
+          <button className="home-button3">
+            <img
+              alt="Rectangle2I674"
+              src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/690b08d2-6137-42cf-89a4-f682e7c689a1/d1791796-b7e9-41b2-aec3-4be3e066be97?org_if_sml=1359"
+              className="home-rectangle23"
+            />
+            <span className="home-text24">
+              <span>Learn more</span>
+            </span>
+          </button>
+          <button className="home-button4">
+            <img
+              alt="Rectangle2I675"
+              src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/690b08d2-6137-42cf-89a4-f682e7c689a1/d7012637-0d52-482b-9c73-b6b1ec4d4978?org_if_sml=1359"
+              className="home-rectangle24"
+            />
+            <span className="home-text26">
+              <span>Learn more</span>
+            </span>
+          </button>
+          <button className="home-button5">
+            <img
+              alt="Rectangle2I676"
+              src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/690b08d2-6137-42cf-89a4-f682e7c689a1/6453b3e3-5f18-4132-ae08-0a3fd8752790?org_if_sml=1359"
+              className="home-rectangle25"
+            />
+            <span className="home-text28">
+              <span>Learn more</span>
+            </span>
+          </button>
+          <button className="home-button6">
+            <img
+              alt="Rectangle2I677"
+              src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/690b08d2-6137-42cf-89a4-f682e7c689a1/e598968b-cc75-49ad-b4e1-819eab02f1d9?org_if_sml=1359"
+              className="home-rectangle26"
+            />
+            <span className="home-text30">
+              <span>Learn more</span>
+            </span>
+          </button>
+          <span className="home-text32 bodyH4">
+            <span>Lorem ipsum dolor</span>
+          </span>
+          <span className="home-text34 bodyP">
+            <span>
+              Amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi.
+            </span>
+          </span>
+          <span className="home-text36 bodyH4">
+            <span>Nemo enim ipsam</span>
+          </span>
+          <span className="home-text38 bodyP">
+            <span>
+              Consequuntur magni dolores eos qui ratione voluptatem sequi
+              nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor
+              sit amet, consectetur, adipisci velit, sed quia non numquam.
+            </span>
+          </span>
+          <span className="home-text40 bodyH4">
+            <span>Tempor incididunt</span>
+          </span>
+          <span className="home-text42 bodyP">
+            <span>
+              Eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
+              est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
+              velit, sed quia non numquam eius modi tempora.
+            </span>
+          </span>
+        </div>
+        <div className="home-body-f">
+          <img
+            alt="Rectangle685"
+            src="/playground_assets/rectangle685-tta.svg"
+            className="home-rectangle01"
+          />
+          <span className="home-text44 bodyH4">
+            <span>Sed ut perspiciatis unde omnis</span>
+          </span>
+          <span className="home-text46 bodyP">
+            <span>
+              Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+              aut fugit, sed quia consequuntur magni dolores eos qui ratione
+              voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem.
+            </span>
+          </span>
+        </div>
+        <div className="home-testimonial-d">
+          <img
+            alt="Rectangle689"
+            src="/playground_assets/rectangle689-w0t.svg"
+            className="home-rectangle02"
+          />
+          <img
+            alt="Rectangle690"
+            src="/playground_assets/rectangle690-fjti.svg"
+            className="home-rectangle03"
+          />
+          <img
+            alt="Rectangle691"
+            src="/playground_assets/rectangle691-v9dh.svg"
+            className="home-rectangle04"
+          />
+          <img
+            alt="Rectangle692"
+            src="/playground_assets/rectangle692-wch6.svg"
+            className="home-rectangle05"
+          />
+          <img
+            alt="Rectangle693"
+            src="/playground_assets/rectangle693-t7a.svg"
+            className="home-rectangle06"
+          />
+          <img
+            alt="Rectangle694"
+            src="/playground_assets/rectangle694-sq7k.svg"
+            className="home-rectangle07"
+          />
+          <img
+            alt="Rectangle695"
+            src="/playground_assets/rectangle695-39vm.svg"
+            className="home-rectangle08"
+          />
+          <img
+            alt="Rectangle696"
+            src="/playground_assets/rectangle696-0s4lk.svg"
+            className="home-rectangle09"
+          />
+          <div className="home-thelargestcommunityofphotoenthusiasts1">
+            <span className="home-text48 sub-PageH1">
+              <span>You’re in good company</span>
+            </span>
+            <span className="home-text50 bodyPLarge">
+              <span>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore
+              </span>
+            </span>
+          </div>
+        </div>
+        <div className="home-signup-c">
+          <span className="home-text52 bodyPLarge">
+            <span>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore
+            </span>
+          </span>
+          <button className="home-button7">
+            <img
+              alt="Rectangle2I610"
+              src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/690b08d2-6137-42cf-89a4-f682e7c689a1/0c725e86-4b8a-4b24-8926-0ce248e7718b?org_if_sml=1446"
+              className="home-rectangle27"
+            />
+            <span className="home-text54">
+              <span>Join Today</span>
+            </span>
+          </button>
+          <button className="home-button8">
+            <img
+              alt="Rectangle2I610"
+              src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/690b08d2-6137-42cf-89a4-f682e7c689a1/796f2c30-8edc-48f4-832d-ca81c6ed9c9c?org_if_sml=1409"
+              className="home-rectangle28"
+            />
+            <span className="home-text56">
+              <span>Contact us</span>
+            </span>
+          </button>
+        </div>
+        <div className="home-footer-c">
+          <span className="home-text58">
+            <span>Mobile app</span>
+          </span>
+          <span className="home-text60">
+            <span>© Photo, Inc. 2019. We love our users!</span>
+          </span>
+          <img
+            alt="Ellipse6107"
+            src="/playground_assets/ellipse6107-9g8b.svg"
+            className="home-ellipse1"
+          />
+          <img
+            alt="Ellipse6108"
+            src="/playground_assets/ellipse6108-2gs2.svg"
+            className="home-ellipse2"
+          />
+          <img
+            alt="Ellipse6109"
+            src="/playground_assets/ellipse6109-pwx7.svg"
+            className="home-ellipse3"
+          />
+          <img
+            alt="Ellipse6110"
+            src="/playground_assets/ellipse6110-rwo.svg"
+            className="home-ellipse4"
+          />
+          <span className="home-text62">
+            <span>Community</span>
+          </span>
+          <span className="home-text64">
+            <span>Company</span>
+          </span>
+          <span className="home-text66">
+            <span>Help desk</span>
+          </span>
+          <span className="home-text68">
+            <span>Blog</span>
+          </span>
+          <span className="home-text70">
+            <span>Resources</span>
+          </span>
+          <div className="home-sample-logo1">
+            <span className="home-text72">
+              <span>photo</span>
+            </span>
+            <img
+              alt="UnionI611"
+              src="/playground_assets/unioni611-t2qk.svg"
+              className="home-union2"
+            />
+            <img
+              alt="UnionI611"
+              src="/playground_assets/unioni611-yqra.svg"
+              className="home-union3"
+            />
+          </div>
+          <img
+            alt="Rectangle6117"
+            src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/690b08d2-6137-42cf-89a4-f682e7c689a1/e0327a32-708b-4804-a831-1a8103a93535?org_if_sml=1138"
+            className="home-rectangle10"
+          />
+        </div>
+        <div className="home-body-e">
+          <img
+            alt="Rectangle6119"
+            src="/playground_assets/rectangle6119-5hg.svg"
+            className="home-rectangle11"
+          />
+          <span className="home-text74 bodyH4">
+            <span>Sed ut perspiciatis unde omnis</span>
+          </span>
+          <span className="home-text76 bodyP">
+            <span>
+              Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+              aut fugit, sed quia consequuntur magni dolores eos qui ratione
+              voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem.
+            </span>
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
